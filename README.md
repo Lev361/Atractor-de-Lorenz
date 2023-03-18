f0b21f39f84b14e313b796363c3facd6e53c3f0d
Lev361 <levimaycon361@gmail.com>
2023-03-18 14:41:08 -0300

    Atractor Lorenz

---
+++ README.md
@@ -0,0 +1,31 @@
+
+# Atractor de Lorenz
+
+O Atractor de Lorenz foi introduzido por Edward Lorenz em 1963, que o derivou a partir das equações simplificadas de rolos de convecção que ocorrem nas equações da atmosfera. É um mapa caótico que mostra como o estado de um sistema dinâmico evolui no tempo num padrão complexo, não-repetitivo e cuja forma é conhecida por se assemelhar a uma borboleta.
+
+Trata-se de um sistema não-linear, tridimensional e determinístico que exibe comportamento caótico e demonstra aquilo a que hoje se chama um atractor estranho.
+
+
+
+
+
+## Exemplos
+
+A trajectória do sistema de Lorenz para valores de ρ=28, σ = 10, β = 8/3
+
+![App Screenshot](https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/Lorenz_attractor_yb.svg/300px-Lorenz_attractor_yb.svg.png)
+
+
+## As equações que governam o Atractor de Lorenz são: 
+
+![App Scheenshot](https://wikimedia.org/api/rest_v1/media/math/render/svg/47c8953623e9bfd87c40f42b0f626d23cd7b52fc)
+![App Scheenshot](https://wikimedia.org/api/rest_v1/media/math/render/svg/d85de949568e0f52bb3961917cd1a82626b23e82)
+![App Scheenshot](https://wikimedia.org/api/rest_v1/media/math/render/svg/ee5564b8c63eef18c0fdad673fa3f04ae8fbe5eb)
+
+ 
+
+
+
+em que a σ \sigma se chama o número de Prandtl e a ρ \rho se chama o número de Rayleigh. Todos os σ \sigma, ρ \rho , β \beta > 0, mas usualmente σ \sigma = 10, β \beta = 8/3, enquanto ρ \rho varia. O sistema exibe comportamento caótico para ρ \rho = 28 mas tem órbitas periódicas para outros valores de ρ \rho.
+
+
--- main.js
+++ main.js
@@ -7,6 +7,14 @@ const sigma = 10
 const rho = 28
 const beta = 8 / 3
 
+
+/**
+ * A função "integrate" recebe um objeto com as propriedades "x", "y" e "z",
+ * que representam as variáveis do modelo de Lorenz. Ela usa essas variáveis
+ * para calcular novos valores de "x", "y" e "z" com base em fórmulas matemáticas
+ * específicas, usando valores pré-definidos de sigma, rho e beta, que são constantes
+ * usadas no modelo de Lorenz.
+ */
 function integrate({
     x,
     y,
@@ -22,6 +30,17 @@ function integrate({
     }
 }
 
+/**
+ * A função "extendPath" recebe dois parâmetros: "path" e "steps". "path" é uma matriz que contém
+ * pontos no espaço tridimensional, e "steps" é o número de etapas adicionais que desejamos adicionar ao caminho.
+ * A função cria um loop com o número de etapas desejadas e itera a cada etapa. A cada iteração,
+ * a função calcula o próximo ponto no caminho adicionando um ponto ao final do caminho existente.
+ * Para fazer isso, a função chama outra função chamada "integrate"
+ * passando o último ponto do caminho atual como um parâmetro.
+ * O objetivo do cálculo feito pela função "integrate" é prever a
+ * posição do próximo ponto no caminho, com base em um modelo matemático chamado de
+ * algoritmo de Lorenz.
+ */
 function extendPath(path, steps) {
     [...Array(steps)].forEach(() => {
         const lastP = path[path.length - 1];
@@ -31,6 +50,33 @@ function extendPath(path, steps) {
     return path
 }
 
+
+/**
+ * 
+ * Essa função chamada "scale" recebe dois parâmetros: "points" e "size". "points"
+ * é uma matriz de pontos tridimensionais que representam um objeto geométrico, e "size"
+ * é um fator de escala que indica o tamanho da geometria resultante.
+ * 
+ * A função começa encontrando os valores mínimos e máximos das coordenadas x, y e z de
+ * todos os pontos na matriz "points". Em seguida, ela armazena esses valores mínimos e
+ * máximos em variáveis separadas chamadas mx, Mx, my, My, mz e Mz. Essas variáveis representam
+ * os limites do objeto geométrico original nos eixos x, y e z.
+ * 
+ * Essas informações são usadas para calcular o centro do objeto geométrico original,
+ * que é o ponto médio em cada eixo (x, y e z). Isso é feito dividindo a soma dos valores
+ * mínimos e máximos por dois.
+ * 
+ * A função retorna um objeto que contém as informações sobre a geometria resultante.
+ * As propriedades do objeto são "points", que contém os pontos do objeto geométrico resultante,
+ * "center", que contém as coordenadas do centro do objeto resultante, e "size", que contém o tamanho
+ * do objeto resultante.
+ * 
+ * Em resumo, a função "scale" encontra os valores mínimos e máximos das coordenadas x, y e z
+ * dos pontos tridimensionais de um objeto geométrico e calcula o centro desse objeto. Em seguida,
+ * ela retorna um objeto que contém informações sobre a geometria resultante, que é uma versão
+ * escalonada do objeto original com base no fator de escala fornecido.
+ * 
+ */
 function scale(points, size) {
     const mx = Math.min(...points.map(({
         x,
@@ -63,6 +109,18 @@ function scale(points, size) {
         z
     }) => z))
 
+
+    /**
+     * 
+     * A função "s" é definida como uma função de seta que recebe três argumentos:
+     * "v", "mv" e "Mv". A função também utiliza a variável "size", que é uma constante
+     * que não é passada como argumento.
+     *
+     * O objetivo dessa função é calcular uma escala de tamanho para um valor "v" com base 
+     * nos valores mínimos e máximos "mv" e "Mv". Essa escala é usada para dimensionar 
+     * um objeto com base em um tamanho especificado.
+     * 
+     */
     const s = (v, mv, Mv) => size * (v - mv) / (Mv - mv)
 
     return points.map(({
@@ -78,10 +136,14 @@ function scale(points, size) {
             y,
             z
         }
-    })
-}
+    })}
 var q = 0
 
+
+/**
+ * A função "draw" recebe um argumento "path", que é um array de pontos.
+ * Esses pontos descrevem uma trajetória que será desenhada no canvas.
+ */
 function draw(path) {
     q += 0.01
     const map = ({
@@ -101,6 +163,13 @@ const path = [{
     z: 1
 }]
 
+/**
+ * A função "step" é responsável por atualizar o canvas a cada quadro de animação.
+ * Ela começa limpando o canvas usando a função "ctx.clearRect()", que recebe quatro argumentos:
+ * as coordenadas x e y do canto superior esquerdo do retângulo a ser limpo, e as coordenadas x e y
+ * do canto inferior direito do mesmo retângulo. Nesse caso, a função limpa todo o canvas,
+ * usando as dimensões do próprio canvas (0, 0, canvas.width, canvas.height).
+ */
 function step() {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     extendPath(path, 10)
